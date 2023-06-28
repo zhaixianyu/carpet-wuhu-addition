1d195fdfb464b2f8c9f6f4ad93437ce3dd3b853e
zhaixianyu <1141927681@qq.com>
2023-06-28 18:40:07 +0800

    1.20

--- src/main/java/com/zxy/remoteinventory01/mixin/MixinLootableContainerBlockEntity.java
+++ src/main/java/com/zxy/remoteinventory01/mixin/MixinLootableContainerBlockEntity.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
