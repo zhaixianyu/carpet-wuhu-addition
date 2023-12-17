d14e62ab847daee5f60c89bee7b370de430ea2fd
zhaixianyu <1141927681@qq.com>
2023-12-17 19:20:27 +0800

    20

--- src/main/java/com/zxy/remoteinventory01/mixin/MixinServerWorld.java
+++ src/main/java/com/zxy/remoteinventory01/mixin/MixinServerWorld.java
@@ -5,6 +5,7 @@ import com.zxy.remoteinventory01.TickList;
 import net.minecraft.block.BlockState;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.ChunkPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,21 +13,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 import static com.zxy.remoteinventory01.OpenInventoryPacket.playerlist;
+import static com.zxy.remoteinventory01.OpenInventoryPacket.tickmap;
 
 @Mixin(ServerWorld.class)
 public class MixinServerWorld {
     @Inject(at = @At("HEAD"),method = "tick")
     public void tick(CallbackInfo ci){
         for (ServerPlayerEntity s : playerlist) {
-            TickList list = OpenInventoryPacket.tickmap.get(s);
-            list.block.scheduledTick(list.state,list.world,list.pos,list.world.random);
-            BlockState state =  list.state;
-            BlockState state2 = list.world.getBlockState(list.pos);
-            if(!state.equals(state2)){
-                OpenInventoryPacket.openFail(s);
+            TickList list = tickmap.get(s);
+            if (!list.world.isChunkLoaded(ChunkPos.toLong(list.pos))) {
+                list.world.scheduleBlockTick(list.pos, list.block, 1);
+                BlockState state = list.state;
+                BlockState state2 = list.world.getBlockState(list.pos);
+                if (!state.equals(state2)) {
+                    OpenInventoryPacket.openFail(s);
+                }
             }
-//            if(list.world.getBlockState(list.pos).equals(list.state)) s.closeHandledScreen();
         }
+//        for (ServerPlayerEntity s : playerlist) {
+//            TickList list = OpenInventoryPacket.tickmap.get(s);
+//            list.block.scheduledTick(list.state,list.world,list.pos,list.world.random);
+//            BlockState state =  list.state;
+//            BlockState state2 = list.world.getBlockState(list.pos);
+//            if(!state.equals(state2)){
+//                OpenInventoryPacket.openFail(s);
+//            }
+////            if(list.world.getBlockState(list.pos).equals(list.state)) s.closeHandledScreen();
+//        }
     }
-
 }
