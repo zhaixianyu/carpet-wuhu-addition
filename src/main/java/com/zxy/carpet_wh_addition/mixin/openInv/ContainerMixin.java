84a8d017024c369c377fa46c3e979cea61c6a6d1
zhaixianyu <1141927681@qq.com>
2026-05-19 12:00:05 +0800

    迁移映射

---
+++ src/main/java/com/zxy/carpet_wh_addition/mixin/openInv/ContainerMixin.java
@@ -0,0 +1,9 @@
+package com.zxy.carpet_wh_addition.mixin.openInv;
+
+
+import com.zxy.carpet_wh_addition.mixin.DummyClass;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(DummyClass.class)
+public interface ContainerMixin {
+}
